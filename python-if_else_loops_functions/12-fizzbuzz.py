891d64bb15555a5a7777367d84f3ffa295a4d253
Eltun Jalilli <118074011+EltunLTN@users.noreply.github.com>
2026-03-17 21:52:51 +0400

    Fix formatting and spacing in fizzbuzz function

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,8 +1,9 @@
+#!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1,101):
+    for i in range(1, 101):
         if i % 3 == 0:
-            i="Fizz"
+            i = "Fizz"
         elif i % 5 == 0:
             i = "Buzz"
-        print(i,end=" ")
+        print(i, end=" ")
 fizzbuzz()
