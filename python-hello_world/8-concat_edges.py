15a60dde133de84de8f2eaf399905303af6a2f27
Eltun <eltunjalilli@gmail.com>
2026-03-17 15:10:24 +0000

    8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[39:67]+str[107:112]+str[:6])
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(str[40:59] + " " + str[60:71] + " " + str[72:83])
