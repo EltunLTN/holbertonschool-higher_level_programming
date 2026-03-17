9a075e818b28d5b177e1ccb7ccc3a04d8498f547
Eltun Jalilli <118074011+EltunLTN@users.noreply.github.com>
2026-03-17 21:58:19 +0400

    Refactor fizzbuzz function to print results directly

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-def fizzbuzz():
-    for i in range(1, 101):
-        if i % 15 == 0:
-            i = "FizzBuzz"
-        elif i % 3 == 0:
-            i = "Fizz"
-        elif i % 5 == 0:
-            i = "Buzz"
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz", end=" ")
+    elif i % 3 == 0:
+        print("Fizz", end=" ")
+    elif i % 5 == 0:
+        print("Buzz", end=" ")
+    else:
         print(i, end=" ")
-fizzbuzz()
