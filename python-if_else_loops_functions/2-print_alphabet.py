35e3e5e074f7d44124b8305948f5a7c40bcebb50
Eltun Jalilli <118074011+EltunLTN@users.noreply.github.com>
2026-03-18 09:21:05 +0400

    Replace prime number check with alphabet print

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
-for n in range(2, 10):
-    for x in range(2, n):
-        if n % x == 0:
-            print(f"{n} equals {x} * {n//x}")
-            break
+#!/usr/bin/python3
+for i in range(97, 123):
+    print('{:c}'.format(i), end='')
