cbef451bb73b0398f11f633f85111c841835a878
Eltun Jalilli <118074011+EltunLTN@users.noreply.github.com>
2026-03-18 09:22:10 +0400

    Refactor print_comb2 to format numbers correctly

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(0,100):
-    if i / 10 < 1:
-        i=f"0{i}"
-    print(i,end=", ")
+for i in range(100):
+    if i < 99:
+        print('{:02d}, '.format(i), end='')
+    else:
+        print(i)
