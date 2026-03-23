5125b4dd165b4cab4faedd20b3ffadcb1ea11451
EltunLTN <eltunjalilli@gmail.com>
2026-03-23 12:29:40 +0000

    m

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new = []
     for i in my_string:
-        if i != 'c' or i!= 'C':
-            print(i, end='')
+        if i != 'c' and i != 'C':
+            new.append(i)
+    return ''.join(new)
