af5baf5cab31883ee3ba4970aa6f9863dd28f319
Eltun Jalilli <118074011+EltunLTN@users.noreply.github.com>
2026-03-23 09:43:52 +0400

    Refactor print statements to use format method

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -4,7 +4,7 @@ from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     a = 10
     b = 5
-    print(f"{a} + {b} = {add(a, b)}")
-    print(f"{a} - {b} = {sub(a, b)}")
-    print(f"{a} * {b} = {mul(a, b)}")
-    print(f"{a} / {b} = {div(a, b)}")
+    print("{a} + {b} = {add(a, b)}".format(a=a, b=b, add=add, sub=sub, mul=mul, div=div))
+    print("{a} - {b} = {sub(a, b)}".format(a=a, b=b, add=add, sub=sub, mul=mul, div=div))
+    print("{a} * {b} = {mul(a, b)}".format(a=a, b=b, add=add, sub=sub, mul=mul, div=div))
+    print("{a} / {b} = {div(a, b)}".format(a=a, b=b, add=add, sub=sub, mul=mul, div=div))
