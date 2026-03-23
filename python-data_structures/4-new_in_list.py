f50c7226c36d0771dd5601a8830d84068e085d29
EltunLTN <eltunjalilli@gmail.com>
2026-03-23 12:15:23 +0000

    m

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,6 +2,6 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list.copy()
-    old=my_list.copy()
+    old = my_list.copy()
     old[idx] = element
     return old
