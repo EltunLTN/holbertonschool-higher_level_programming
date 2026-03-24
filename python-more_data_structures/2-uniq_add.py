40631473af79dded52b85b44333f0817f5f15f75
Eltun Jalilli <eltunjalilli@gmail.com>
2026-03-24 17:09:12 +0400

    2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -3,6 +3,3 @@ def uniq_add(my_list=[]):
     result = set(my_list)
     new_list = list(result)
     return sum(new_list)
-my_list = [1, 2, 3, 1, 4, 2, 5]
-result = uniq_add(my_list)
-print("Result: {:d}".format(result))
