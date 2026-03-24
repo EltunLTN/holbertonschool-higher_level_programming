e935c6c4fc1f03a8715d29835b8bef17aa20a50c
Eltun Jalilli <eltunjalilli@gmail.com>
2026-03-24 11:42:18 +0400

    11

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,11 @@
+def delete_at(my_list=[], idx=0):
+    if idx < 0:
+        return my_list
+    new_list=[]
+    for i in range(len(my_list)):
+        if i != idx:
+            new_list.append(my_list[i])
+    my_list.clear()
+    for i in range(len(new_list)):
+        my_list.append(new_list[i])
+    return new_list
