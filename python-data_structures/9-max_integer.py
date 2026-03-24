f8e788e9b56226104d2dd1115f34ab5e9eeacd01
Eltun Jalilli <eltunjalilli@gmail.com>
2026-03-24 10:04:17 +0400

    9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == 0:
+    if my_list == []:
         return None
     for i in range(1, len(my_list)):
         if my_list[i-1] > my_list[i]:
