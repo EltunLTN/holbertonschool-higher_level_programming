7acd4cfe57b9278dc6f679dcb75a1101bc3a5478
Eltun Jalilli <eltunjalilli@gmail.com>
2026-03-24 17:21:57 +0400

    5

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,4 @@
+def print_sorted_dictionary(a_dictionary):
+    return sorted(a_dictionary)
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+print(a_dictionary)
