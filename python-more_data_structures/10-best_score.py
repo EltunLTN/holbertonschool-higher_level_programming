350754d7e7ff6dcfabbcdfcd5523f22ea0811739
Eltun Jalilli <eltunjalilli@gmail.com>
2026-03-25 09:24:24 +0400

    10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,8 +1,5 @@
+#!/usr/bin/python3
 def best_score(a_dictionary):
-    pass
-        
-
-
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
+    if not a_dictionary:
+        return None
+    return max(a_dictionary, key=lambda k: a_dictionary[k])
