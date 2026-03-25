b912946aca13f8b5cc3daac0054c3c927855b549
Eltun Jalilli <eltunjalilli@gmail.com>
2026-03-25 08:38:00 +0400

    6

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,8 @@
+def best_score(a_dictionary):
+    pass
+        
+
+
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if roman_string is None or len(roman_string) == 0:
+        return 0
+    total = 0
+    for i in range(len(roman_string)):
+        if i < len(roman_string) -1 and roman[roman_string[i]] < roman[roman_string[i+1]]:
+            total -= roman[roman_string[i]]
+        else:
+            total += roman[roman_string[i]]
+    return total
