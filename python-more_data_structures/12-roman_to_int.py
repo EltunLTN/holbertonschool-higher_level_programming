bd397e337ec576a041c010ed8cf8e2030264b04d
Eltun Jalilli <eltunjalilli@gmail.com>
2026-03-25 08:39:31 +0400

    12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if roman_string is None or len(roman_string) == 0:
+    if roman_string is None or not isinstance(roman_string, str):
         return 0
     total = 0
     for i in range(len(roman_string)):
