a0c95323451c52ee5a4dbc24abbf269df97bfb06
Eltun Jalilli <eltunjalilli@gmail.com>
2026-03-30 21:44:02 +0400

    s

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
+def safe_print_integers(my_list=[], x=0):
     count = 0
-
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        except (TypeError, ValueError):
-            pass
-
+        except (ValueError, TypeError):
+            continue
     print()
     return count
