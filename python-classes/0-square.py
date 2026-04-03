a724eafe76db763fee0f0587aad434f23ad8fd8a
Eltun Jalilli <eltunjalilli@gmail.com>
2026-04-03 09:39:55 +0400

    0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""This module defines a Square class that represents a square shape."""
 
 
 class Square:
+    """This class defines a square by its properties and future behaviors."""
     pass
