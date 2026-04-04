c0a595413caef13154742714412c6b5989a64e8f
Eltun Jalilli <eltunjalilli@gmail.com>
2026-04-04 09:34:46 +0400

    exception adv

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
+"""Defines a function that safely prints an integer."""
+import sys
+
+
 def safe_print_integer_err(value):
-    
+    """Print an integer with error handling.
+
+    Args:
+        value: The value to be printed.
+
+    Returns:
+        True if the value is an integer and has been printed, False otherwise.
+    """
     try:
         print("{:d}".format(value))
         return True
-    except (ValueError, TypeError) as error:
-        print("Exception:", error)
+    except Exception as e:
+        print("Exception:", e, file=sys.stderr)
         return False
