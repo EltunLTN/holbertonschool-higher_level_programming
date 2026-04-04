3fbf7cb95accd9ec0b39acda4220c316a13cd16e
Eltun Jalilli <eltunjalilli@gmail.com>
2026-04-04 22:54:46 +0400

    102

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""This module defines a function that performs a magic calculation."""
-def magic_calculation(a, b):
-    """Perform a magic calculation based on the values of a and b."""
-    result = 0
+"""This module defines a magic_calculation
+function based on Python bytecode."""
 
+
+def magic_calculation(a, b):
+    """Perform a magic calculation based on the given bytecode."""
+    result = 0
     for i in range(1, 3):
         try:
             if i > a:
-                raise Exception("Too far")
-            result += (a ** b) / i
+                raise Exception('Too far')
+            result += a ** b / i
         except Exception:
-            result = a + b
+            result = b + a
             break
-
     return result
