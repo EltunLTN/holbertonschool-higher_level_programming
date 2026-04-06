353b57cea935f53f591a3c39c9e6b33f033cf6fb
Eltun Jalilli <eltunjalilli@gmail.com>
2026-04-06 21:28:03 +0400

    0

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""This module defines a function that returns the list of available attributes and methods of an object."""
+def lookup(obj):
+    """Returns the list of available attributes and methods of an object."""
+    return dir(obj)
