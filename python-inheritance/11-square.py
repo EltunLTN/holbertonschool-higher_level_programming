7301be83a63b075afb9553375ad8e62eadbf496c
Eltun Jalilli <eltunjalilli@gmail.com>
2026-04-07 18:50:37 +0400

    11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module that defines a Square class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class Square that inherits from Rectangle"""
+
+    def __init__(self, size):
+        """Initialize square with given size"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Returns the area of the square"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Returns the string representation of the square"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
