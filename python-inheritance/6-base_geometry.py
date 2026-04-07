11d768257dbe73489885671ba240c3f15bb64a85
Eltun Jalilli <eltunjalilli@gmail.com>
2026-04-07 14:06:13 +0400

    6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """An empty class representing base geometry."""
+    def area(self):
+        raise ValueError("area() is not implemented")
