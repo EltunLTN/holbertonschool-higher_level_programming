12f0cecf911e0e6fea756aca0641f3d6584aaa51
Eltun Jalilli <eltunjalilli@gmail.com>
2026-04-10 12:20:10 +0400

    00

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,1 +1,28 @@
-.
+#!/usr/bin/python3
+"""This module defines an abstract class and its subclasses."""
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """Abstract class representing an animal."""
+
+    @abstractmethod
+    def sound(self):
+        """Abstract method to be implemented by subclasses."""
+        pass
+
+
+class Dog(Animal):
+    """Class representing a dog, inheriting from Animal."""
+
+    def sound(self):
+        """Return the sound made by a dog."""
+        return "Bark"
+
+
+class Cat(Animal):
+    """Class representing a cat, inheriting from Animal."""
+
+    def sound(self):
+        """Return the sound made by a cat."""
+        return "Meow"
