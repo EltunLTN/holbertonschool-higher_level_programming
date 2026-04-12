f2c8305532ee5a67f963bbcae4a4604d4c206e7c
Eltun Jalilli <eltunjalilli@gmail.com>
2026-04-12 08:05:10 +0400

    01

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Module that defines shapes using abstract base classes and duck typing."""
+
 from abc import ABC, abstractmethod
 from math import pi
 
@@ -25,16 +26,6 @@ class Circle(Shape):
         """Initialize a circle with a radius."""
         self.radius = radius
 
-    @property
-    def radius(self):
-        """Return the radius."""
-        return self.__radius
-
-    @radius.setter
-    def radius(self, value):
-        """Set radius, always storing absolute value."""
-        self.__radius = abs(value)
-
     def area(self):
         """Return the area of the circle."""
         return pi * self.radius ** 2
@@ -51,26 +42,6 @@ class Rectangle(Shape):
         """Initialize a rectangle with width and height."""
         self.width = width
         self.height = height
-
-    @property
-    def width(self):
-        """Return the width."""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """Set width, always storing absolute value."""
-        self.__width = abs(value)
-
-    @property
-    def height(self):
-        """Return the height."""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """Set height, always storing absolute value."""
-        self.__height = abs(value)
 
     def area(self):
         """Return the area of the rectangle."""
