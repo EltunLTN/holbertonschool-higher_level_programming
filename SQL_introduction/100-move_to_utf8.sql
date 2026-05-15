28d6170eebb6c158f8e9e898e600ac3410281ce9
Eltun Jalilli <118074011+EltunLTN@users.noreply.github.com>
2026-05-15 11:10:34 +0400

    Refactor SQL to ensure UTF8 encoding for database

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,14 +1,17 @@
--- Convert database to UTF8
+-- Use the database
+USE hbtn_0c_0;
+
+-- Convert database charset
 ALTER DATABASE hbtn_0c_0
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
--- Convert table to UTF8
+-- Convert table charset
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
--- Convert field name to UTF8
+-- Ensure column is utf8mb4
 ALTER TABLE first_table
 MODIFY name VARCHAR(256)
 CHARACTER SET utf8mb4
